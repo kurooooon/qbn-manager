b53ed3e3f0cab7889d7654283af45f24c0275cd5
kurooooon <b1006110@gmail.com>
2025-03-22 16:47:52 +0900

    新しいDialogコンポーネントを@radix-ui/react-dialogを使用して実装し、関連するストーリーを追加しました。また、package.jsonとpackage-lock.jsonに依存関係を追加しました。

---
+++ src/components/ui/dialog.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./button";
+import { Dialog } from "./dialog";
+
+const meta = {
+  component: Dialog,
+  args: {
+    isOpen: true,
+    title: "title",
+    content: "context",
+    primaryButton: <Button variant="default">primary</Button>,
+    secondaryButton: <Button variant="secondary">secondary</Button>,
+  },
+} satisfies Meta<typeof Dialog>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
