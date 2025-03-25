0db0cda74e038d39d2139c7c191c572d0c61466e
kurooooon <b1006110@gmail.com>
2025-03-25 20:55:30 +0900

    package.jsonとpackage-lock.jsonに新しい依存関係「happy-dom」を追加し、Vitestのテスト環境を「happy-dom」に変更しました。また、storybookTestプラグインの設定を更新しました。

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,9 +1,14 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { defineWorkspace } from "vitest/config";
 
-// const dirname =
-//   typeof __dirname !== "undefined"
-//     ? __dirname
-//     : path.dirname(fileURLToPath(import.meta.url));
+import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
+
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineWorkspace([
@@ -13,21 +18,21 @@ export default defineWorkspace([
     test: {
       name: "unit",
       include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
-      environment: "node",
+      environment: "happy-dom",
     },
   },
-  // TODO: storybookTestを有効にする
-  // {
-  //   extends: "vite.config.ts",
-  //   plugins: [
-  //     // The plugin will run tests for the stories defined in your Storybook config
-  //     // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-  //     storybookTest({ configDir: path.join(dirname, ".storybook") }),
-  //   ],
-  //   test: {
-  //     name: "storybook",
-  //     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
-  //     environment: "happy-dom",
-  //   },
-  // },
+  {
+    extends: "vite.config.ts",
+    plugins: [storybookTest({ configDir: path.join(dirname, ".storybook") })],
+    test: {
+      name: "storybook",
+      browser: {
+        enabled: true,
+        headless: true,
+        provider: "playwright",
+        instances: [{ browser: "chromium" }],
+      },
+      setupFiles: [".storybook/vitest.setup.ts"],
+    },
+  },
 ]);
